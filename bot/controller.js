60e11d45bbfaddbec01f3c24e608b0f0ffbac5e0
Michael Morrissey <mikehmorrissey@gmail.com>
2016-06-26 23:42:07 -0400

    add temp questions and survey

--- bot/controller.js
+++ bot/controller.js
@@ -1,5 +1,5 @@
 import TwilioSMSBot from 'botkit-sms'
-import ddp from './ddp'
+import dictionary from './dictionary'
 
 const controller = TwilioSMSBot({
   account_sid: process.env.TWILIO_ACCOUNT_SID,
@@ -12,16 +12,57 @@ const port = process.env.PROD ? process.env.PORT : 4000
 
 controller.setupWebserver(port, (err, webserver) => {
   controller.createWebhookEndpoints(controller.webserver, bot, () => {
-    console.log('started', bot)
+    console.log('started')
   })
 })
 
-controller.hears('.*', 'message_received', (bot, message) => {
-  console.log(message)
-  ddp.call('submitResponse', [{ body: message.text, sms: true, phone: message.channel }], (err, response) => {
-    bot.reply(message, { text: response })
+const tempQuestions = [
+  { question: 'Are you a trump supporter?' },
+  { question: 'Do you believe in god?' },
+  { question: 'Do you exercise?' },
+  { question: 'Do you like dogs?' },
+]
+
+const nextQuestion = (convo, bot, questions, index) => {
+  if (questions[index + 1]) {
+    askQuestion(convo, bot, questions, index + 1)
+  } else {
+    convo.say(dictionary.results)
+  }
+}
+
+const askQuestion = (convo, bot, questions, index = 0) => {
+  convo.ask(questions[index].question, [
+    {
+      pattern: bot.utterances.yes,
+      callback(response, convo) {
+        nextQuestion(convo, bot, questions, index)
+        convo.next()
+      }
+    },
+    {
+      pattern: bot.utterances.no,
+      callback(response, convo) {
+        nextQuestion(convo, bot, questions, index)
+        convo.next()
+      }
+    },
+    {
+      default: true,
+      callback(response, convo) {
+        convo.repeat()
+        convo.next()
+      }
+    }
+  ])
+}
+
+controller.hears('hi', 'message_received', (bot, message) => {
+  bot.startConversation(message, (err, convo) => {
+    convo.say(dictionary.introduction)
+    convo.say(dictionary.explanation)
+    askQuestion(convo, bot, tempQuestions)
   })
 })
-
 
 export default controller
---
+++ bot/dictionary.js
@@ -0,0 +1,7 @@
+const dictionary = {
+  introduction: 'Hi, I\'m Dealbreaker. We\'re going to play a quick game to see if you\'re worthy of getting sara\'s number.',
+  explanation: 'I\'ll ask you a series of yes or no questions. Be honest and good luck',
+  results: 'Thanks! processing the results now',
+}
+
+export default dictionary
